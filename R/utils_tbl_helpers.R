e4609d2a62e6c29adc5eebffd6cae9648c31ae3e
Lefkios Paikousis <lefkiospaikousis@yahoo.co.uk>
2020-10-28 17:15:35 +0200

    cleanup

--- R/utils_tbl_helpers.R
+++ R/utils_tbl_helpers.R
@@ -1,29 +1,4 @@
 
-# some tables
-
-
-# foodex.1 <- readxl::read_xlsx("SampleData/Foodex.1.xlsx") %>%
-#   select(
-#     -ends_with("_HCODE"),
-#     -ends_with("_ID")
-#   ) %>%
-#   mutate(across(everything(), as.factor))
-# 
-# 
-# 
-# sample_consumption <- 
-#   readxl::read_xlsx("SampleData/consumption_sample.xlsx") %>% 
-#   janitor::clean_names()
-
-# foodex.1 <- improrisk.shiny:::foodex.1
-# sample_consumption <- improrisk.shiny:::sample_consumption
-
-
-# tbl_foodex_desc <- 
-#   foodex.1 %>% 
-#   distinct(FOODEX_L1_DESC, FOODEX_L2_DESC, FOODEX_L3_DESC, FOODEX_L4_DESC) %>% 
-#   relocate(FOODEX_L1_DESC, FOODEX_L2_DESC, FOODEX_L3_DESC, FOODEX_L4_DESC)
-
 
 fdx1_group_level1 <- c("FOODEX_L1_DESC")
 fdx1_group_level2 <- c("FOODEX_L1_DESC", "FOODEX_L2_DESC")
@@ -48,28 +23,6 @@ fdx1_levels_cons <- c(
 )
 
 
-
-# tbl_unique_level3 <- 
-#   foodex.1 %>% 
-#   distinct(FOODEX_L3_DESC, FOODEX_L2_DESC, FOODEX_L1_DESC)
-
-# fdx1_l1 <- unique(foodex.1$FOODEX_L1_DESC)
-# fdx1_l2 <- unique(foodex.1$FOODEX_L2_DESC)
-# fdx1_l3 <- unique(foodex.1$FOODEX_L3_DESC)
-# fdx1_l4 <- unique(foodex.1$FOODEX_L4_DESC)
-# 
-# 
-# 
-# sub_info <- 
-#   data.frame(
-#     stringsAsFactors = FALSE,
-#     check.names = FALSE,
-#     
-#     row.names = c("Chemical Substance", "Substance Category","Reference value (μg/Kg b.w.)",
-#                   "Type of Reference value","Frequency"),
-#     values = c("Mercury (Hg)","Contaminant",
-#                "4.00","Tolerable Intake","WEEKLY")
-#   )
 
 
 var_names <- list(
@@ -202,60 +155,6 @@ info_improrisk <- tagList(
     
     
 )
-
-
-# Demo data ####
-
-
-#  TODO Need to use a reactive value in the server for these..
-# ref_value <- 4
-# exposure_factor = 7
-# 
-# consumption_days <- 
-#   sample_consumption %>% 
-#   dplyr::group_by(subjectid) %>% 
-#   dplyr::summarise(cons_days  = dplyr::n_distinct(day)) %>% 
-#   dplyr::ungroup()
-# 
-# 
-# tbl_subjects <- 
-#   sample_consumption %>% 
-#   dplyr::distinct(
-#     subjectid, 
-#     gender, 
-#     age,
-#     weight,
-#     area,
-#     pop_class,
-#     wcoeff
-#   ) %>% 
-#   dplyr::left_join(consumption_days)
-# 
-# 
-# tbl_exposure <- 
-#   sample_consumption %>% 
-#   dplyr::group_by(
-#     subjectid
-#   ) %>% 
-#   dplyr::summarise(
-#     nday_lb = sum(wcoeff_adjusted_refined_exposure_lb, na.rm = TRUE),
-#     nday_mb = sum(wcoeff_adjusted_refined_exposure_mb, na.rm = TRUE),
-#     nday_ub = sum(wcoeff_adjusted_refined_exposure_ub, na.rm = TRUE)
-#   ) %>% 
-#   dplyr::left_join(tbl_subjects) %>% 
-#   dplyr::mutate(
-#     dplyr::across(
-#       dplyr::starts_with(("nday_")), ~ ./wcoeff
-#     )
-#   ) %>% 
-#   dplyr::mutate(
-#     subExp_LB = (nday_lb/cons_days) * exposure_factor,
-#     subExp_MB = (nday_mb/cons_days) * exposure_factor,
-#     subExp_UB = (nday_ub/cons_days) * exposure_factor
-#   ) %>% 
-#   dplyr::ungroup()
-
-
 
 # Sortable ####
 
